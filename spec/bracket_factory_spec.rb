c4ee4aaa46180877c2d8877a2204b22b7e10d7d9
tamerl <eng.tamereid@gmail.com>
2020-03-09 20:17:28 +1100

    refactoring the code, better implementation

--- spec/bracket_factory_spec.rb
+++ spec/bracket_factory_spec.rb
@@ -4,15 +4,26 @@ require './src/bracket_factory.rb'
 describe '#bracket_factory' do
   context 'Adds the missing bracket in the right sequence,' do
     it 'returns this string [{(B)}{(B)(B)}] when entering [{(B}{(B)(B)}]' do
-      expect(bracket_factory("[{(B}{(B)(B)}]")).to eq("[{(B)}{(B)(B)}]")
+      string=BracketFactory.new('[{(B}{(B)(B)}]')
+      expect(string.find_missing_brackets).to eq("[{(B)}{(B)(B)}]")
     end
     it 'returns this string [{(B)}{(B)(B)}] when entering {(B)}{(B)(B)}]' do
-      expect(bracket_factory("{(B)}{(B)(B)}]")).to eq("[{(B)}{(B)(B)}]")
+      string=BracketFactory.new('{(B)}{(B)(B)}]')
+      expect(string.find_missing_brackets).to eq("[{(B)}{(B)(B)}]")
+    end
+    it 'returns this string [{(B)}{(B)(B)}] when entering [{(B)}(B)(B)}]' do
+      string=BracketFactory.new('[{(B)}(B)(B)}]')
+      expect(string.find_missing_brackets).to eq("[{(B)}{(B)(B)}]")
+    end
+    it 'returns this string [{(B)}{(B)(B)}] when entering [{(B)}{(B(B)}]' do
+      string=BracketFactory.new('[{(B)}{(B(B)}]')
+      expect(string.find_missing_brackets).to eq("[{(B)}{(B)(B)}]")
     end
 
     it 'raises an error if the input is not in the brackets format' do
       expect do
-        bracket_factory(nil)
+        string=BracketFactory.new(nil)
+        string.find_missing_brackets
       end.to raise_error("Please enter a valid bracket string")
     end
   end
--- src/bracket_factory.rb
+++ src/bracket_factory.rb
@@ -1,24 +1,26 @@
-def bracket_factory(str)
-  raise "Please enter a valid bracket string" if str == nil
-  @brackets = { 0 => '[]' , 1 => '{}' , 2 => '()'}
-  close = []
-  str.split('').each_with_index do |char,ind|
-    if ['[','(','['].include?(char)
-      close << [char,ind]
-    elsif [']',')','}'].include?(char)
-      return '['+str if close.length == 0
-      recent = close.pop
-      if char != @brackets.find {|key,val| val.include?(recent.first)}[1].split('').last
-        if weight(char) < weight(recent.first)
-          return str.insert(ind,@brackets[weight(recent.first)].split('').last)
-        else
-          return str.insert(recent.last+1,@brackets[weight(char)].split('').first)
-        end
-      end
+class BracketFactory
+
+  def initialize(str)
+    @str = str
+  end
+
+  def find_missing_brackets
+    raise "Please enter a valid bracket string" if @str == nil
+    bees = @str.split('').each_index.select{ |char| @str.split('')[char] == "B"}
+    bees.reverse!
+    for x in bees do
+      @str = @str.split('').insert(x-1,'(').join() if @str.split('')[x-1] != "("
+      @str = @str.split('').insert(x+1,')').join() if @str.split('')[x+1] != ")"
     end
+    self.add_missing_curl
+  end
+
+  def add_missing_curl
+    box = @str.split("(B)")
+    box[0] = "[{" if box[0] == "{"
+    box[-1] = "}]" if box[-1] == "}"
+    box = box.each_with_index{|char,index| box[index]="}{" if char == "{"}
+    box = box.each_with_index{|char,index| box[index]="}{" if char == "}"}
+    p box.join("(B)")
   end
 end
-
-  def weight (char)
-    return @brackets.find {|key,val| val.include?(char)}[0]
-  end
